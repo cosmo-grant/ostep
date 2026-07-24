6dd9caa42bf8c9b05b09b47e8dd8f67d2ebdc10d
Cosmo Grant <cosmo.grant@postcodelottery.co.uk>
2026-07-24 16:17:04 +0100

    wip

--- snippets/memory-user.c
+++ snippets/memory-user.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int n = atoi(argv[1]) * 1024 * 1024 / sizeof(int);
+  size_t n = (size_t)atoi(argv[1]) * 1024 * 1024 / sizeof(int);
   int *a = malloc(n * sizeof(int));
   while (1)
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
       a[i] += 1;
 }
+
+/*
+* free shows that the memory isn't used immediately.
+* It takes a second or two, presumably because only becomes used when touched.
+* It becomes available immediately on killing the program though.
+*
+* Allocating 1000 Mb reduced MemFree by 1009, not 1000.
+* The rest of the program takes up some space.
+*
+* The numbers reported by free vary a bit (a few mb) even when doing nothing.
+*/ 
